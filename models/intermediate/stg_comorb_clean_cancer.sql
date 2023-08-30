724510a9ff93a59b07c454fffb0a3cada9aa97ac
jbawagon@gmail.com <jbawagon@gmail.com>
2023-08-30 15:59:10 +0000

    Update with a new colonne status (actif ou sous surveillance)

--- models/intermediate/stg_comorb_clean_cancer.sql
+++ models/intermediate/stg_comorb_clean_cancer.sql
@@ -1,5 +1,9 @@
 {{config(materialized = 'table')}}
 
 SELECT *
+,CASE
+  WHEN patho_niv3 LIKE "%acti%" THEN "actif"
+  WHEN patho_niv3 LIKE "%sous surveillance" THEN "sous-surveillance"
+END AS status
 FROM `pathologie.dbt_staging.stg_comorb_clean` 
 WHERE patho_niv1="Cancers"
