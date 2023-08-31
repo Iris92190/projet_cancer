7d9b0c878c13633804f35b7ea0737506d2679b69
jbawagon@gmail.com <jbawagon@gmail.com>
2023-08-31 08:11:29 +0000

    update pour retirer les lignes où patho niv 2 et patho niv 3 sont null

--- models/staging/column_clean_combord.sql
+++ models/staging/column_clean_combord.sql
@@ -1,2 +1,3 @@
 select annee,patho_niv1,patho_niv2,patho_niv3,libelle_comorbidite,Ncomorb,Ntop,Proportion_comorb
 from{{source('pathologie','comorbidites')}}
+WHERE patho_niv3 IS NOT NULL AND patho_niv2 IS NOT NULL
