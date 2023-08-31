8a6235e2dac3efadd0696b5f7ee9a4d5ef12b02c
jbawagon@gmail.com <jbawagon@gmail.com>
2023-08-31 08:23:36 +0000

    update

--- models/intermediate/comorb_clean_cancer.sql
+++ models/intermediate/comorb_clean_cancer.sql
@@ -5,5 +5,5 @@ SELECT *
   WHEN patho_niv3 LIKE "%acti%" THEN "actif"
   WHEN patho_niv3 LIKE "%sous surveillance" THEN "sous-surveillance"
 END AS status
-FROM `pathologie.dbt_staging.stg_comorb_clean` 
+FROM {{ref("column_clean_combord")}}
 WHERE patho_niv1="Cancers"
