e3865ae4ba103573ed60296c4ac291e5a0d99306
jbawagon@gmail.com <jbawagon@gmail.com>
2023-08-31 08:14:59 +0000

    update materialize tablea

--- models/staging/column_clean_combord.sql
+++ models/staging/column_clean_combord.sql
@@ -1,3 +1,5 @@
+{{config(materialized = 'table')}}
+
 select annee,patho_niv1,patho_niv2,patho_niv3,libelle_comorbidite,Ncomorb,Ntop,Proportion_comorb
 from{{source('pathologie','comorbidites')}}
 WHERE patho_niv3 IS NOT NULL AND patho_niv2 IS NOT NULL
