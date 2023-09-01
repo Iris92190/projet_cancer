a04221c005833d97a82072bb4a5ae33c2a8f3d2a
jbawagon@gmail.com <jbawagon@gmail.com>
2023-09-01 09:22:21 +0000

    update lineage regarding cancern comorb clean

--- models/intermediate/comorb_clean_cancer.sql
+++ models/intermediate/comorb_clean_cancer.sql
@@ -5,5 +5,5 @@ SELECT *
   WHEN patho_niv3 LIKE "%acti%" THEN "actif"
   WHEN patho_niv3 LIKE "%sous surveillance" THEN "sous-surveillance"
 END AS status
-FROM {{ref("column_clean_combord")}}
+FROM {{ref("stg_comorb_clean")}}
 WHERE patho_niv1="Cancers"
